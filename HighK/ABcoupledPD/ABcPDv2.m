a4887212b18323c11a728895b5f64f3e2d7e0c3f
Liaro0903 <ian880903@gmail.com>
2022-07-01 14:27:55 -0400

    Typo

--- HighK/ABcoupledPD/ABcPDv2.m
+++ HighK/ABcoupledPD/ABcPDv2.m
@@ -5,7 +5,7 @@
 clear;
 close all;
 
-load('FindABcPD_rand_high_2.mat');
+load('FindABcPD_rand_1.mat');
 
 parameters_to_vary = {
   'AB.ACurrent.gbar', ...
@@ -56,7 +56,7 @@ x.integrate;
 % Try to run this in parallel
 % Turn on following if you want to do a bunch of params together
 % tic
-model = 2100; % debug mode
+model = 39; % debug mode
 % parfor model = 1:10 % debug mode
 % parfor model = 1:length(params) % full version
 
@@ -67,16 +67,16 @@ x.set(parameters_to_vary, params(:, model));
 all_ABPD_V = zeros(100000, 4);
 
 % Part 1: Integrate pre-connection
-% results_and_spiketimes = x.integrate;
-% all_ABPD_V(1:50000, 1) = results_and_spiketimes.AB.V;
-% all_ABPD_V(1:50000, 2) = results_and_spiketimes.PD.V;
+results_and_spiketimes = x.integrate;
+all_ABPD_V(1:50000, 1) = results_and_spiketimes.AB.V;
+all_ABPD_V(1:50000, 2) = results_and_spiketimes.PD.V;
 
 % Under construction, trying to find frequency
-% spike_threshold = -40;
-% metrics_AB = xtools.V2metrics(results_and_spiketimes.AB.V, 'spike_threshold', spike_threshold, 'debug', true, 'sampling_rate', 1);
-% n_spikes = xtools.findNSpikes(results_and_spiketimes.AB.V, spike_threshold);
-% spiketimes = xtools.findNSpikeTimes(results_and_spiketimes.AB.V, n_spikes, spike_threshold);
-% metrics_PD = xtools.V2metrics(results_and_spiketimes.PD.V, 'sampling_rate', 1);
+spike_threshold = -40;
+metrics_AB = xtools.V2metrics(results_and_spiketimes.AB.V, 'spike_threshold', spike_threshold, 'debug', true, 'sampling_rate', 1);
+n_spikes = xtools.findNSpikes(results_and_spiketimes.AB.V, spike_threshold);
+spiketimes = xtools.findNSpikeTimes(results_and_spiketimes.AB.V, n_spikes, spike_threshold);
+metrics_PD = xtools.V2metrics(results_and_spiketimes.PD.V, 'sampling_rate', 1);
 
 % Part 2: Integrate with connection
 x1 = copy(x);
@@ -85,9 +85,9 @@ base = 110;
 x1.connect('AB','PD', synapse_type, 'gmax', base * 4);
 x1.connect('PD','AB', synapse_type, 'gmax', base);
 
-% results_and_spiketimes = x1.integrate;
-% all_ABPD_V(50001:100000, 1) = results_and_spiketimes.AB.V;
-% all_ABPD_V(50001:100000, 2) = results_and_spiketimes.PD.V;
+results_and_spiketimes = x1.integrate;
+all_ABPD_V(50001:100000, 1) = results_and_spiketimes.AB.V;
+all_ABPD_V(50001:100000, 2) = results_and_spiketimes.PD.V;
 
 % Under construction, trying to find frequency
 % metrics_AB = xtools.V2metrics(results_and_spiketimes.AB.V, 'spike_threshold', spike_threshold, 'debug', true, 'sampling_rate', 1);
@@ -96,11 +96,11 @@ x1.connect('PD','AB', synapse_type, 'gmax', base);
 % metrics_PD = xtools.V2metrics(results_and_spiketimes.PD.V, 'debug', true, 'sampling_rate', 1);
 
 % Part 3: Integrate at high K
-x1.t_end = 10000;
-x1 = setPotential(x1, -56, 24, ["AB" "PD"]);
-results_and_spiketimes = x1.integrate;
-all_ABPD_V(1:100000, 1) = results_and_spiketimes.AB.V;
-all_ABPD_V(1:100000, 2) = results_and_spiketimes.PD.V;
+% x1.t_end = 10000;
+% x1 = setPotential(x1, -56, 24, ["AB" "PD"]);
+% results_and_spiketimes = x1.integrate;
+% all_ABPD_V(1:100000, 3) = results_and_spiketimes.AB.V;
+% all_ABPD_V(1:100000, 4) = results_and_spiketimes.PD.V;
 
 % Part 4: Integrate at changing gbars
 % x1.set('PD.ACurrent.gbar', 5000);
@@ -110,15 +110,20 @@ all_ABPD_V(1:100000, 2) = results_and_spiketimes.PD.V;
 % all_ABPD_V(50001:100000, 4) = results_and_spiketimes.PD.V;
 
 % Part 5: Put it back to -80
-x1 = setPotential(x1, -80, 24, ["AB" "PD"]);
-results_and_spiketimes = x1.integrate;
-all_ABPD_V(1:100000, 3) = results_and_spiketimes.AB.V;
-all_ABPD_V(1:100000, 4) = results_and_spiketimes.PD.V;
+% x1 = setPotential(x1, -80, 24, ["AB" "PD"]);
+% results_and_spiketimes = x1.integrate;
+% all_ABPD_V(1:100000, 3) = results_and_spiketimes.AB.V;
+% all_ABPD_V(1:100000, 4) = results_and_spiketimes.PD.V;
 
-x.myplot2({['Model ', int2str(model)]}, all_ABPD_V, {'AB80', 'PD80', 'AB70', 'PD70'});
+x.myplot2({['Model ', int2str(model)]}, all_ABPD_V, {'AB80', 'PD80'});
 % saveas(x1.handles.fig, ['./data/FindABcPD_rand_1/', num2str(model), '.png']);
 
 % close all;
 
 % end
 % t = toc;
+
+
+% Debug tools
+% x1.plot();
+% x1.manipulate();
