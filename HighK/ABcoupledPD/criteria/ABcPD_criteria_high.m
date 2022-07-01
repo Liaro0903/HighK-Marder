f0483fc637a9819aaf495384599dbc2e9dc489c9
Liaro0903 <ian880903@gmail.com>
2022-07-01 16:58:15 -0400

    Update criteria_high

--- HighK/ABcoupledPD/criteria/ABcPD_criteria_high.m
+++ HighK/ABcoupledPD/criteria/ABcPD_criteria_high.m
@@ -2,17 +2,20 @@ function qualifies = ABcPD_criteria_high(x)
   arguments
     x (1,1) xolotl
   end
-  pause(2);
+  % pause(2);
   x = setPotential(x, -56, 24, ["AB" "PD"]);
-  [AB_isBursting, PD_isBursting, ~, ~] = meta_criteria(x, "bursting", "bursting");
-  disp([num2str(AB_isBursting), ' | ', num2str(PD_isBursting)]);
-  x.myplot('hi');
-  if (AB_isBursting && PD_isBursting)
+  [AB_isBursting, PD_isBursting, ~, PD_V] = meta_criteria(x, "bursting", "bursting");
+  metrics_PD = xtools.V2metrics(PD_V(50001:100000), 'sampling_rate', 1);
+  % disp([num2str(AB_isBursting), ' | ', num2str(PD_isBursting), ' | ', num2str(metrics_PD.n_spikes_per_burst_mean)]);
+  % x.myplot('hi');
+  if (AB_isBursting && PD_isBursting && 1 < metrics_PD.n_spikes_per_burst_mean)
     x = setPotential(x, -80, 24, ["AB" "PD"]);
-    [AB_isBursting, PD_isBursting, ~, ~] = meta_criteria(x, "bursting", "bursting");
-    if (AB_isBursting && PD_isBursting)
-      x.myplot('hi2');
-      disp('Found burst at -80');
+    [AB_isBursting, PD_isBursting, ~, PD_V] = meta_criteria(x, "bursting", "bursting");
+    metrics_PD = xtools.V2metrics(PD_V(50001:100000), 'sampling_rate', 1);
+    % disp(num2str(metrics_PD.n_spikes_per_burst_mean));
+    if (AB_isBursting && PD_isBursting && 1 < metrics_PD.n_spikes_per_burst_mean)
+      % x.myplot('hi2');
+      % disp('Found burst at -80');
       qualifies = 1.0; 
     else
       qualifies = 0.0;
