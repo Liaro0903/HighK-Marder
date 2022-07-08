ebc9cecfc369fb40f72fe438ac4bf1ba570a458e
Liaro0903 <ian880903@gmail.com>
2022-07-08 01:24:00 -0400

    Better criteria for rand_search

--- HighK/ABcoupledPD/criteria/ABcPD_criteria.m
+++ HighK/ABcoupledPD/criteria/ABcPD_criteria.m
@@ -6,11 +6,19 @@ function qualifies = ABcPD_criteria(x)
   [AB_isWave, PD_isTonic, AB_V, PD_V] = meta_criteria(x, "bursting", "tonic");
   metrics_AB = xtools.V2metrics(AB_V(50001:100000), 'spike_threshold', -40, 'sampling_rate', 1);
   metrics_PD = xtools.V2metrics(PD_V(50001:100000), 'sampling_rate', 1);
-  % disp([num2str(AB_isWave), ' | ', num2str(PD_isTonic) ' | ', num2str(metrics_AB.firing_rate), ' | ', num2str(metrics_PD.firing_rate)]);
+  % disp([
+  %   num2str(AB_isWave), ' | ', num2str(PD_isTonic), ...
+  %   ' | ', num2str(metrics_AB.firing_rate), ' | ', num2str(metrics_PD.firing_rate), ...
+  %   ' | ', num2str(metrics_PD.n_spikes_per_burst_mean), ' | ', num2str(metrics_PD.isi_std) ...
+  % ]);
   % all_ABPD_V = zeros(x.t_end * 10, 2);
   % all_ABPD_V(1:50000, 1) = AB_V(1:50000);
   % all_ABPD_V(1:50000, 2) = PD_V(1:50000);
-  if (AB_isWave && PD_isTonic && 0.5 < metrics_AB.firing_rate && metrics_AB.firing_rate < 1.5 && 3 < metrics_PD.firing_rate && metrics_PD.firing_rate < 10)
+  if (AB_isWave && ...
+    0.5 < metrics_AB.firing_rate && metrics_AB.firing_rate < 1.5 && ...
+    3 < metrics_PD.firing_rate && metrics_PD.firing_rate < 20 && ...
+    metrics_PD.n_spikes_per_burst_mean <= 1.1 && metrics_PD.isi_std < 200 ...
+  ) % restrict isi_std so isi are consistent not having 2 spikes that are close (which is more like a bursts than tonic)
     x1 = copy(x);
     synapse_type = 'Electrical';
     base = 110;
@@ -20,7 +28,7 @@ function qualifies = ABcPD_criteria(x)
     if (AB_isWave && PD_isBursting)
       % all_ABPD_V(50001:100000, 1) = AB_V(1:50000);
       % all_ABPD_V(50001:100000, 2) = PD_V(1:50000);
-      % x.myplot2({'hi'}, all_ABPD_V);
+      % x.myplot2({[num2str(metrics_PD.firing_rate), ' | ', num2str(metrics_PD.isi_std)]}, all_ABPD_V);
       qualifies = 1.0;
     else
       qualifies = 0.0;
--- HighK/ABcoupledPD/criteria/ABcPD_criteria_high.m
+++ HighK/ABcoupledPD/criteria/ABcPD_criteria_high.m
@@ -4,23 +4,68 @@ function qualifies = ABcPD_criteria_high(x)
   end
   % pause(2);
   x = setPotential(x, -56, 24, ["AB" "PD"]);
-  [AB_isBursting, PD_isBursting, ~, PD_V] = meta_criteria(x, "bursting", "bursting");
-  metrics_PD = xtools.V2metrics(PD_V(50001:100000), 'sampling_rate', 1);
-  % disp([num2str(AB_isBursting), ' | ', num2str(PD_isBursting), ' | ', num2str(metrics_PD.n_spikes_per_burst_mean)]);
+  results_and_spiketimes = x.integrate;
+  AB_V = results_and_spiketimes.AB.V(50001:100000);
+  PD_V = results_and_spiketimes.PD.V(50001:100000);
+
+  max_n_spikes_per_burst = 0;
+  max_n_spikes_per_burst_V = 10;
+  for spike_threshold = -40:5:0
+    metrics_AB = xtools.V2metrics(AB_V, 'sampling_rate', 1, 'spike_threshold', spike_threshold);
+    if max_n_spikes_per_burst < metrics_AB.n_spikes_per_burst_mean
+      max_n_spikes_per_burst = metrics_AB.n_spikes_per_burst_mean;
+      max_n_spikes_per_burst_V = spike_threshold;
+    end 
+  end
+  metrics_AB = xtools.V2metrics(AB_V, 'sampling_rate', 1, 'spike_threshold', max_n_spikes_per_burst_V);
+  metrics_PD = xtools.V2metrics(PD_V, 'sampling_rate', 1);
+  % disp([
+  %   'n_spikes_PD: ', num2str(metrics_PD.n_spikes_per_burst_mean) ...
+  %   ' | PD_ibi_mean: ', num2str(metrics_PD.ibi_mean) ...
+  %   ' | PD_ibi_std: ', num2str(metrics_PD.ibi_std) ...
+  %   ' | n_spikes_AB: ', num2str(metrics_AB.n_spikes_per_burst_mean), ...
+  %   ' | ibi_std: ', num2str(metrics_AB.ibi_std) ...
+  %   ' | AB_ibi_mean: ', num2str(metrics_AB.ibi_mean) ...
+  % ]);
   % x.myplot('hi');
-  if (AB_isBursting && PD_isBursting && 1 < metrics_PD.n_spikes_per_burst_mean)
+  if ( ...
+    1 < metrics_PD.n_spikes_per_burst_mean && metrics_PD.ibi_mean > 500 && ...
+    metrics_AB.n_spikes_per_burst_mean >= 3 && metrics_AB.ibi_std < 100 ...
+  )
+    % disp('Found bursts at -56');
     x = setPotential(x, -80, 24, ["AB" "PD"]);
-    [AB_isBursting, PD_isBursting, ~, PD_V] = meta_criteria(x, "bursting", "bursting");
-    metrics_PD = xtools.V2metrics(PD_V(50001:100000), 'sampling_rate', 1);
-    % disp(num2str(metrics_PD.n_spikes_per_burst_mean));
-    if (AB_isBursting && PD_isBursting && 1 < metrics_PD.n_spikes_per_burst_mean)
-      % x.myplot('hi2');
+    results_and_spiketimes_80 = x.integrate;
+    AB_V_80 = results_and_spiketimes_80.AB.V(50001:100000);
+    PD_V_80 = results_and_spiketimes_80.PD.V(50001:100000);
+
+    max_n_spikes_per_burst = 0;
+    max_n_spikes_per_burst_V = 10;
+    for spike_threshold = -40:5:0
+      metrics_AB_80 = xtools.V2metrics(AB_V_80, 'sampling_rate', 1, 'spike_threshold', spike_threshold);
+      if max_n_spikes_per_burst < metrics_AB_80.n_spikes_per_burst_mean
+        max_n_spikes_per_burst = metrics_AB_80.n_spikes_per_burst_mean;
+        max_n_spikes_per_burst_V = spike_threshold;
+      end 
+    end
+    metrics_AB_80 = xtools.V2metrics(AB_V_80, 'sampling_rate', 1, 'spike_threshold', max_n_spikes_per_burst_V);
+    metrics_PD_80 = xtools.V2metrics(PD_V_80, 'sampling_rate', 1);
+    % disp([
+    %   'n_spikes_PD: ', num2str(metrics_PD_80.n_spikes_per_burst_mean) ...
+    %   ' | PD_ibi_mean: ', num2str(metrics_PD_80.ibi_mean) ...
+    %   ' | PD_ibi_std: ', num2str(metrics_PD_80.ibi_std) ...
+    %   ' | n_spikes_AB: ', num2str(metrics_AB_80.n_spikes_per_burst_mean), ...
+    %   ' | ibi_std: ', num2str(metrics_AB_80.ibi_std) ...
+    %   ' | AB_ibi_mean: ', num2str(metrics_AB_80.ibi_mean) ...
+    % ]);
+    % x.myplot('hi2');
+    % pause(10);
+    if (1 < metrics_AB_80.n_spikes_per_burst_mean && 500 < metrics_PD_80.ibi_mean && 1 < metrics_PD_80.n_spikes_per_burst_mean)
       % disp('Found burst at -80');
       qualifies = 1.0; 
     else
       qualifies = 0.0;
     end
-  else
+  else 
     qualifies = 0.0;
   end
 end
