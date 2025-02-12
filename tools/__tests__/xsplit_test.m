c6b8f6462fb35a01c692dfddf42a45f5846e9b25
Liaro0903 <ian880903@gmail.com>
2025-02-11 22:45:49 -0500

    Added xsplit

--- tools/__tests__/xsplit_test.m
+++ tools/__tests__/xsplit_test.m
@@ -6,10 +6,11 @@ hx.add_pyloric_neuron('ABPD', 'PD', 'prinz-ac', 1);
 target_conds = hx.x.find('*ac_shift_*');
 cond_pairs = nchoosek(target_conds, 2);
 
-disp(cond_pairs(3, :));
-[neuron_name, conds, gates, cond_gates] = xsplit(cond_pairs(3, :))
+disp(cond_pairs(3, :)); % output PD.ACurrent.ac_shift_h, PD.CaS.ac_shift_m
+[neuron_name, conds, gates, cond_gates, gate_conds] = xsplit(cond_pairs(3, :));
 
 assert(neuron_name == 'PD', 'Case failed: expected PD');
 assert(isequal(conds, ["ACurrent", "CaS"]), 'Case failed: expected ["ACurrent, "CaS"]');
 assert(isequal(gates, ["h", "m"]), 'Case failed: expected ["h", "m"]');
 assert(isequal(cond_gates, ["A.h", "CaS.m"]), 'Case failed: expected ["A.h", "CaS.m"]');
+assert(isequal(gate_conds, ["h_{A}", "m_{CaS}"]), 'Case failed: expected ["h_{A}", "m_{CaS}"]');
--- tools/xsplit.m
+++ tools/xsplit.m
@@ -1,7 +1,8 @@
-function [neuron_name, conds, gates, cond_gates] = xsplit(xconds)
+function [neuron_name, conds, gates, cond_gates, gate_conds] = xsplit(xconds)
   conds = strings(1, length(xconds));
   gates = strings(1, length(xconds));
   cond_gates = strings(1, length(xconds));
+  gate_conds = strings(1, length(xconds));
   for i = 1:length(xconds)
     cond_str = string(xconds(i));
     cond_str = strsplit(cond_str, ".");
@@ -9,5 +10,6 @@ function [neuron_name, conds, gates, cond_gates] = xsplit(xconds)
     conds(i) = cond_str(2);
     gates(i) = strrep(cond_str(3), 'ac_shift_', '');
     cond_gates(i) = strcat(strrep(conds(i), 'Current', ''), '.', gates(i));
+    gate_conds(i) = strcat(gates(i), '_{', strrep(conds(i), 'Current', ''), '}');
   end
 end
